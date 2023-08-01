3a6af4c50c12e892f98438f072de096ff3516783
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2023-07-31 23:46:22 -0400

    Create POSITION.lua

---
+++ POSITION.lua
@@ -0,0 +1,41 @@
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(-275.966187, 106.502579, 1082.27722, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
+---
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(3186.74902, 186.074615, -445.307434, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
+---
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(-304.634949, 109.502579, -1299.92542, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
+---
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(955.227844, 102.842445, -710.717163, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
+---
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(-890.572937, 107.502579, 286.706909, 0.906307817, 0, -0.42261824, 0, 1, 0, 0.42261824, 0, 0.906307817)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
+---
+local Platform = Instance.new("Part")
+Platform.Parent = workspace
+Platform.Anchored = true
+Platform.CFrame = CFrame.new(1216.32666, 185.074615, 3344.21851, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+Platform.Size = Vector3.new(41, 1, 40)
+Platform.CanCollide = true
