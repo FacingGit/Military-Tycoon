efc0ed7b55bc88a948c167c29cf134785a8b3f43
FacingGit (FacingNoOneEXE) <72046783+FacingGit@users.noreply.github.com>
2023-08-01 00:25:43 -0400

    Update POSITION.lua

--- POSITION.lua
+++ POSITION.lua
@@ -4,6 +4,7 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(-275.966187, 106.502579, 1082.27722, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
 ---
 local Platform = Instance.new("Part")
 Platform.Parent = workspace
@@ -11,6 +12,7 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(3186.74902, 186.074615, -445.307434, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
 ---
 local Platform = Instance.new("Part")
 Platform.Parent = workspace
@@ -18,6 +20,7 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(-304.634949, 109.502579, -1299.92542, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
 ---
 local Platform = Instance.new("Part")
 Platform.Parent = workspace
@@ -25,6 +28,7 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(955.227844, 102.842445, -710.717163, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
 ---
 local Platform = Instance.new("Part")
 Platform.Parent = workspace
@@ -32,6 +36,7 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(-890.572937, 107.502579, 286.706909, 0.906307817, 0, -0.42261824, 0, 1, 0, 0.42261824, 0, 0.906307817)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
 ---
 local Platform = Instance.new("Part")
 Platform.Parent = workspace
@@ -39,3 +44,4 @@ Platform.Anchored = true
 Platform.CFrame = CFrame.new(1216.32666, 185.074615, 3344.21851, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Platform.Size = Vector3.new(41, 1, 40)
 Platform.CanCollide = true
+Platform.Name = "QC2LMRiv8N"
